d4ac5762da04b2704f8d2593de5ecbce39fb89f4
LiyouWang <1085066875@qq.com>
2020-03-06 14:14:23 +0800

    using cg method

--- demo_use_scipy_optimize.py
+++ demo_use_scipy_optimize.py
@@ -4,10 +4,13 @@ sys.path.append("S3DGLPy")
 from S3DGLPy.PolyMesh import *
 from Util.util import *
 import time
+from scipy.optimize import least_squares
 
 def func_res(x, L, b):
     X = x.reshape(-1, 3)
-    return np.sqrt(np.sum(np.square(L.dot(X)-b), axis=1)).mean()
+    loss = np.sqrt(np.sum(np.square(L.dot(X)-b), axis=0)).mean()#.flatten()
+    print(loss)
+    return loss
 
 
 if __name__ == "__main__":
@@ -20,9 +23,12 @@ if __name__ == "__main__":
     delta = load_pickle_file(r"./data/delta.pkl")
     start = time.time()
     x = polymesh.VPos
-    res = scipy.optimize.minimize(func_res, x.reshape(-1), args=(L, delta), method='BFGS', jac=False,
-                                  options={"gtol": 1e-7, "maxiter": 1000, "disp":True, "eps": 1e-7})
+    res = scipy.optimize.minimize(func_res, x0=x.reshape(-1), args=(L, delta), method='CG', jac=False,
+                                  options={"gtol": 1e-7, "maxiter": 200, "disp":True, "eps": 1e-3})
+    # res = least_squares(func_res, x0=x.reshape(-1), args=(L, delta), method='trf')
+    print(res.success)
     print(res.x)
+    print(res.cost)
     polymesh.VPos = res.x.reshape(-1, 3)
     # for i in range(3):
     #     polymesh.VPos[:, i] = lsqr(L, delta[:, i])[0]
